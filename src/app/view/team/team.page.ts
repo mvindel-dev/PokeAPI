ad8307123a04865a71f9873b30422c31bb5278e9
mvindel_dev <marcvindeldev@gmail.com>
2024-04-06 21:51:08 +0200

    reading qr's

--- src/app/view/team/team.page.ts
+++ src/app/view/team/team.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-team',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamPage implements OnInit {
 
-  constructor() { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
 }
